45399271c0b1e9bac93ba01b5164abc5e7e2653a
importnetminecraft <devonrutledge03@gmail.com>
2020-06-03 17:05:31 -0600

    Added basic player tracker

--- InputMod.js
+++ InputMod.js
@@ -7,16 +7,31 @@ const modifier = (text) => {
   if (!state.initialized) {
     state.step = 0
     state.initialized = true
+    state.players = []
   }
+  
+  if(state.step === 0){
+    state.pin = Math.floor( Math.random() * 10000 )
+    state.message = state.pin.toString()
+  }else{
+    state.message = ""
+  }
+  
   let modifiedText = ""
   const lowered = text.toLowerCase()
-  if (/[\s\n]+>/.test(text) || state.step <= 0) {
+  if (/^[\s\n]+>\s/.test(text) || state.step <= 0) {
     modifiedText = text
+    charName = text.replace(/^[\s\n]+>\s/, "").match(/\w+\s/)[0]
+    if(!state.players.includes(charName)){
+      state.players.push(charName)
+      state.message += charName + "has Joined the Game"
+    }
   } else {
     state.disableOut = true
   }
+  
   state.step++
-  // You must return an object with the text property defined. 
+  // You must return an object with the text property defined.
   return { text: modifiedText }
 }
 
