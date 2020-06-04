c7088b14726a206585fb271eb0bb982e72a37958
L00PIL00P <tim.plata@outlook.de>
2020-06-04 16:23:05 +0200

    Improved "you"-filter to target the differend forms of "you" more precisely

--- OutputMod.js
+++ OutputMod.js
@@ -4,25 +4,38 @@
 
 const modifier = (text) => {
 
-  let modifiedText = ""
-
-  if (state.disableOut) {
-    state.disableOut = false
-  } else { 
-	for (sentence of text.split(".")) {
-	  let lower = sentence.toLowerCase()
-	  let dialog = false
-	  for (quote of lower.split("\"")) {
-		  if (dialog) {
-			  lower = lower.replace(quote, "")
-		  }
-		  dialog = !dialog
-	  }
-	  if (!(lower.includes("you") || lower.includes("your")) && /\S/.test(lower)) {
-		modifiedText += sentence + "."
-	  }
+	let modifiedText = ""
+	let shapesOfYou = ["you", "your", "yourself", "you're", "you'll", "you'd"]
+	let punctuation = [" ", ".", ",", "!", "?", ":", ";", "'", "\""]
+	if (state.disableOut) {
+		state.disableOut = false
+	} else { 
+		for (sentence of text.split(".")) {
+			let lower = sentence.toLowerCase()
+			let dialog = false
+			for (quote of lower.split("\"")) {
+				if (dialog) {
+					lower = lower.replace(quote, "")
+				}
+				dialog = !dialog
+			}
+			let containsYou = false
+			for (youShape of shapesOfYou) {
+				if (lower.endsWith(youShape)) {
+					containsYou = true
+				} else {
+					for (symbol of punctuation) {
+						if (lower.includes(" "+youShape+symbol)) {
+							containsYou = true
+						}
+					}
+				}
+			}
+			if (!containsYou && /\S/.test(lower)) {
+				modifiedText += sentence + "."
+			}
+		}
 	}
-  }
 
   // You must return an object with the text property defined. 
   return { text: modifiedText }
